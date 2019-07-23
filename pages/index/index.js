96a2074b85afe49d5405fa9bc34b0c86601ffe48
cardal1 <534666612@qq.com>
2019-07-23 10:40:30 +0800

    测试“ git commit -m 测试“

---
+++ pages/index/index.js
@@ -0,0 +1,96 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    dataList: [
+      {
+        goods_id: 1,
+        goods_title: '物品标题1',
+        goods_img: '/images/wallet.png',
+        person_name: '谢振轩',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }, {
+        goods_id: 1,
+        goods_title: '物品标题2',
+        goods_img: '/images/wallet.png',
+        person_name: '吴斌峰',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }, {
+        goods_id: 1,
+        goods_title: '物品标题3',
+        goods_img: '/images/wallet.png',
+        person_name: '杨宗霖',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }, {
+        goods_id: 1,
+        goods_title: '物品标题4',
+        goods_img: '/images/wallet.png',
+        person_name: '熊景涛',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }, {
+        goods_id: 1,
+        goods_title: '物品标题5',
+        goods_img: '/images/wallet.png',
+        person_name: '方思政',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }
+    ],
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  release: function () {
+    wx.navigateTo({
+      url: '../find/release/things',
+    })
+  }
+})
