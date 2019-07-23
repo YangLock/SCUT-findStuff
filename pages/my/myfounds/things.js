aea7ba09cb795c1833305b8726321d3d8b05e8ce
cardal1 <534666612@qq.com>
2019-07-23 19:04:18 +0800

    update myfounds

--- pages/my/myfounds/things.js
+++ pages/my/myfounds/things.js
@@ -1,12 +1,54 @@
-// pages/my/myfounds/things.js
+// pages/my/myfounds/people.js
 var app = getApp()
 Page({
   data: {
-    currentTab: 0
+    currentTab: 0,
+    dataList: [
+      {
+        goods_id: 1,
+        goods_title: '物品标题1',
+        goods_img: '/images/wallet.png',
+        person_name: '谢振轩',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }, {
+        goods_id: 1,
+        goods_title: '物品标题2',
+        goods_img: '/images/wallet.png',
+        person_name: '吴斌峰',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }, {
+        goods_id: 1,
+        goods_title: '物品标题3',
+        goods_img: '/images/wallet.png',
+        person_name: '杨宗霖',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }, {
+        goods_id: 1,
+        goods_title: '物品标题4',
+        goods_img: '/images/wallet.png',
+        person_name: '熊景涛',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }, {
+        goods_id: 1,
+        goods_title: '物品标题5',
+        goods_img: '/images/wallet.png',
+        person_name: '方思政',
+        goods_place: 'A1-308',
+        goods_time: '上午3、4节'
+      }
+    ],
   },
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
-
+    //动态计算高度
+    var line = this.data.dataList.length;
+    this.setData({
+      aheight: 369 * line
+    });
   },
   //滑动切换
   swiperTab: function (e) {
