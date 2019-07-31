37757b6e06cf8ee2ddd1f4c58e9d57e252489573
JackWuo <45913091+JackWuo@users.noreply.github.com>
2019-07-31 22:49:57 +0800

    Add files via upload

--- pages/index/index.js
+++ pages/index/index.js
@@ -53,6 +53,7 @@ Page({
     })
   },
   onLoad: function () {
+    this.get_goods();
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
@@ -80,6 +81,37 @@ Page({
       })
     }
   },
+//从数据库获得新数据
+  get_goods:function(){
+    wx.showLoading({
+      title: '加载中',
+    })
+    var that = this;
+    wx.request({
+      url: 'https://www.4singledogs.cn/get/get_goods',
+      data: {
+        userID: app.globalData.open_id
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "GET",
+      success: function (res) { //成功后根据当前的页面刷新次数进行黏接或重填
+        console.log(res.data);
+          let array2 = res.data;
+          let array1 = that.data.dataList;
+          array1 = array1.concat(array2);
+          that.setData({
+            dataList: array1
+          });
+        
+      },
+      fail: function (res) {
+        wx.hideLoading();
+      }
+    })
+  },
+
   getUserInfo: function(e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
