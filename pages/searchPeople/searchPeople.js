7ecc00f36a64ce72bdd3f927ce842aaca2c27da6
JackWuo <45913091+JackWuo@users.noreply.github.com>
2019-07-31 22:50:55 +0800

    Add files via upload

--- pages/searchPeople/searchPeople.js
+++ pages/searchPeople/searchPeople.js
@@ -49,8 +49,73 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function () {
+    this.get_persons();
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  //从数据库获得新数据
+  get_persons: function () {
+    wx.showLoading({
+      title: '加载中',
+    })
+    var that = this;
+    wx.request({
+      url: 'https://www.4singledogs.cn/get/get_presons',
+      data: {
+        userID: app.globalData.open_id
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      method: "GET",
+      success: function (res) { //成功后根据当前的页面刷新次数进行黏接或重填
+        console.log(res.data);
+        let array2 = res.data;
+        let array1 = that.data.dataList;
+        array1 = array1.concat(array2);
+        that.setData({
+          dataList: array1
+        });
+
+      },
+      fail: function (res) {
+        wx.hideLoading();
+      }
+    })
+  },
+
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
 
   /**
