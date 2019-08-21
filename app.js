a010f6f9cf99a1c4fdd875ee90ee13886db6f36f
cardal1 <534666612@qq.com>
2019-08-22 01:29:19 +0800

    前后端连接

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ App({
     })
   },
   globalData: {
-    userInfo: null
+    userInfo: false,
+    open_id:null,
+    secret: '3af2793e330b5ad415b7bc64dc4a7803',
+    appid: 'wx1f08d278ddf3988e',
+    baseurl:'http://localhost:3000',
+    //goodurl:'http://localhost:3001'
   }
 })
---
+++ utils/generateID.js
@@ -0,0 +1,25 @@
+// 这是一个包含生成三种唯一ID的函数的JS文件
+//  分别为寻物的ID，寻物主的ID，留言的ID
+//   由于它们都是12位整数的数据类型
+//   为了区分以便后面其他功能的开发
+//   以1开头的就是寻物的ID
+//  以2开头的就是寻物主的ID
+//  以3开头的就是留言的ID
+
+function generateGoodID() {
+  return Number('1' + Math.random().toString().substr(3, 5) + Date.now().toString().substr(7, 6));
+}
+
+function generatePersonID() {
+  return Number('2' + Math.random().toString().substr(3, 5) + Date.now().toString().substr(7, 6));
+}
+
+function generateComID() {
+  return Number('3' + Math.random().toString().substr(3, 5) + Date.now().toString().substr(7, 6));
+}
+
+module.exports = {
+  generateGoodID: generateGoodID,
+  generatePersonID: generatePersonID,
+  generateComID: generateComID
+}
